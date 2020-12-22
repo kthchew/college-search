e6d196bb26231a93cd96e9fd5eb579431d66c9b7
Kenneth Chew <kenneth.c0@protonmail.com>
2020-12-21 23:54:48 -0500

    Add tests for Int formatting

---
+++ CollegeSearchTests/IntFormattingTests.swift
@@ -0,0 +1,27 @@
+//
+//  CollegeSearchTests.swift
+//  CollegeSearchTests
+//
+//  Created by Kenneth Chew on 12/21/20.
+//
+
+import XCTest
+@testable import CollegeSearch
+
+class CollegeSearchTests: XCTestCase {
+  func testNormalIntFormatting() throws {
+    XCTAssertEqual("4,329", 4329.normalFormat)
+    XCTAssertEqual("23", 23.normalFormat)
+  }
+  
+  func testCurrencyIntFormatting() throws {
+    XCTAssertEqual("$4,329", 4329.currencyFormat)
+    XCTAssertEqual("$23", 23.currencyFormat)
+  }
+  
+  func testPercentIntFormatting() throws {
+    XCTAssertEqual("34%", 34.percentFormat)
+    XCTAssertEqual("392%", 392.percentFormat)
+    XCTAssertNotEqual("1,000%", 10.percentFormat)
+  }
+}
