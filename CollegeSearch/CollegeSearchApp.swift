bbe72da9ffd23a297320a2c12dc3b4d926f30ec2
Kenneth Chew <kenneth.c0@protonmail.com>
2020-12-21 19:48:30 -0500

    Initial Commit

---
+++ CollegeSearch/CollegeSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollegeSearchApp.swift
+//  CollegeSearch
+//
+//  Created by Kenneth Chew on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct CollegeSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
