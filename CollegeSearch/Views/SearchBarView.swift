6d75d8aa56ecb4ec1a7454ef233de57e51e59f2f
Kenneth Chew <kenneth.c0@protonmail.com>
2020-12-23 14:13:45 -0500

    Hide and show cancel button in search bar automatically, fix appearance in light mode, search automatically after user stops typing in search bar for a short time

--- CollegeSearch/Views/SearchBarView.swift
+++ CollegeSearch/Views/SearchBarView.swift
@@ -17,6 +17,8 @@ struct SearchBarView: UIViewRepresentable {
   
   class Coordinator: NSObject, UISearchBarDelegate {
     let parent: SearchBarView
+    /// A timer used to run the `onSearch` closure when the user has stopped typing for 1 second.
+    var stoppedTypingTimer: Timer?
     
     init(_ parent: SearchBarView) {
       self.parent = parent
@@ -27,18 +29,28 @@ struct SearchBarView: UIViewRepresentable {
       if searchText.count == 0 {
         parent.onClear?()
       } else {
-        parent.onSearch?(searchText)
+        stoppedTypingTimer?.invalidate()
+        stoppedTypingTimer = Timer.scheduledTimer(withTimeInterval: 0.75, repeats: false) { [weak self] _ in
+          self?.parent.onSearch?(searchText)
+        }
       }
+    }
+    
+    // Show the cancel button when the user taps the search bar
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+      searchBar.setShowsCancelButton(true, animated: true)
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
       searchBar.endEditing(false)
+      searchBar.setShowsCancelButton(false, animated: true)
       parent.onSearch?(searchBar.text ?? "")
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
       searchBar.endEditing(false)
-      if searchBar.text?.count ?? 1 > 0 {
+      searchBar.setShowsCancelButton(false, animated: true)
+      if searchBar.text?.count ?? -1 > 0 {
         parent.onSearch?(searchBar.text ?? "")
       } else {
         parent.onClear?()
@@ -50,8 +62,10 @@ struct SearchBarView: UIViewRepresentable {
     let searchBar = UISearchBar()
     searchBar.delegate = context.coordinator
     searchBar.layer.borderWidth = 1 // remove redundant separators on top and bottom of search bar
+    searchBar.layer.borderColor = UIColor.clear.cgColor
     searchBar.placeholder = "Search Schools"
-    searchBar.showsCancelButton = true
+    searchBar.showsCancelButton = false
+    searchBar.text = searchText
     return searchBar
   }
   
